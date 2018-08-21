612207d9161c9649a8a6874ebd84f99036dfba29
Aravind Adithya <aravindadithya95@gmail.com>
2018-08-21 09:52:41 -0400

    Search contacts API

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, request, make_response
-
-import json
+from flask import Flask, request, make_response, jsonify
 
 # import configuration file
 import config
@@ -14,7 +12,7 @@ app = Flask(__name__)
 @app.route('/contact', methods=['GET'])
 def list_contacts():
     """
-    This endpoint will provite a listing of all contacts, allowing for a defined pageSize
+    This endpoint will provide a listing of all contacts, allowing for a defined pageSize
     (number of results allowed back), and the ability to offset by page number to get multiple pages.
     """
 
@@ -31,133 +29,55 @@ def list_contacts():
         return make_response('Invalid page number.', 400)
 
     # read the data store
-    res = data_store.read({
-        'query_string': {
-            'default_field': 'name',
-            'query': query
-        }
-    })
-
-    # extract requested data in range
-    data = []
-    for i in range(page_size * (page - 1), page_size):
-        if (i >= len(res['hits']['hits'])):
-            break
-        data.append(res['hits']['hits'][i]['_source'])
-
-    # send response
-    return make_response(json.dumps(data), 200)
+    return make_response(jsonify(data_store.search_contacts(page_size, page, query)), 200)
 
 
 @app.route('/contact', methods=['POST'])
 def create_contact():
     """This endpoint creates the contact."""
-
-    # check if name exists in the request
-    if 'name' not in request.form or not request.form['name']:
-        return make_response('Invalid POST request', 400)
-
-    # create the document
-    data = {
-        'name': request.form['name'],
-        'phone': request.form['phone'] if 'phone' in request.form else '',
-        'email': request.form['email'] if 'email' in request.form else '',
-        'address': request.form['address'] if 'address' in request.form else ''
-    }
-
-    # read the data store
-    res = data_store.read({
-        'term': {
-            '_id': data['name'].lower()
-        }
-    })
-
-    # check if name already exists
-    if res['hits']['total'] > 0:
-        return make_response('Contact name must be unique.', 400)
-
-    # create new document
-    success = data_store.create(data)
+    
+    # create the contact
+    res = data_store.create_contact(request.form)
 
     # send response
-    if success:
-        return make_response('Contact successfully created.', 201)
-    else:
-        return make_response('Field(s) too long', 400)
+    return make_response(res['description'], res['status'])
 
 
 @app.route('/contact/<string:name>', methods=['GET'])
 def get_contact(name):
     """This endpoint returns the contact details by a unique name."""
 
-    # read the data store
-    res = data_store.read({
-        'term': {
-            '_id': name.lower()
-        }
-    })
+    # read the contact
+    res = data_store.read_contact(name)
 
     # check if it exists
-    if res['hits']['total'] == 0:
+    if res is None:
         return make_response('Contact does not exist.', 404)
 
     # send response
-    return make_response(json.dumps(res['hits']['hits'][0]['_source']), 200)
+    return make_response(jsonify(res), 200)
 
 
 @app.route('/contact/<string:name>', methods=['PUT'])
 def update_contact(name):
     """This endpoint updates the contact by a unique name."""
 
-    # read the data store
-    res = data_store.read({
-        'term': {
-            '_id': name.lower()
-        }
-    })
-
-    # check if it exists
-    if res['hits']['total'] == 0:
-        return make_response('Contact does not exist.', 404)
-
-    data = {}
-    if 'phone' in request.form:
-        data['phone'] = request.form['phone']
-    if 'email' in request.form:
-        data['email'] = request.form['email']
-    if 'address' in request.form:
-        data['address'] = request.form['address']
+    # update the contact
+    res = data_store.update_contact(name, request.form)
     
-    # update contact
-    success = data_store.update(name, data)
-
     # send response
-    if success:
-        return make_response('Contact succesfully updated.', 200)
-    else:
-        return make_response('Field(s) too long', 400)
+    return make_response(res['description'], res['status'])
 
 
 @app.route('/contact/<string:name>', methods=['DELETE'])
 def delete_contact(name):
     """This endpoint deletes the contact by a unique name."""
 
-    # read the data store
-    res = data_store.read({
-        'term': {
-            '_id': name.lower()
-        }
-    })
-
-    # check if it exists
-    if res['hits']['total'] == 0:
-        return make_response('Contact does not exist.', 404)
-
-    # delete contact
-    data_store.delete(name)
-
+    # delete the contact
+    res = data_store.delete_contact(name)
+    
     # send response
-    return make_response('Contact succesfully deleted.', 200)
+    return make_response(res['description'], res['status'])
 
 
 if __name__ == '__main__':
--- es_store.py
+++ es_store.py
@@ -9,65 +9,209 @@ es = Elasticsearch(
 )
 
 
-def create(data, index='address-book', doc_type='contact'):
+def create(index, doc_type, id, doc):
     """Creates a document in the data store."""
-
-    # check bounds
-    if not checkBounds(data):
-        return False
 
     es.create(
         index=index,
         doc_type=doc_type,
-        id=data['name'].lower(),
-        body=data
+        id=id,
+        body=doc
     )
-    return True
 
 
-def read(query, index='address-book', doc_type='contact'):
-    """Searches for documents in the data store."""
+def read(index, doc_type, id):
+    """Searches unique documents in the data store."""
 
-    return es.search(
+    res = es.search(
         index=index,
         doc_type=doc_type,
         body={
-            'query': query
+            'query': {
+                'term': {
+                    '_id': id
+                }
+            }
         }
     )
 
+    return res['hits']['hits'][0]['_source'] if res['hits']['total'] > 0 else None
 
-def update(id, query, index='address-book', doc_type='contact'):
+
+def update(index, doc_type, id, doc):
     """Updates document in the data store."""
-
-    # check bounds
-    if not checkBounds(query):
-        return False
 
     es.update(
         index=index,
         doc_type=doc_type,
-        id=id.lower(),
+        id=id,
         body={
-            'doc': query
+            'doc': doc
         }
     )
-    return True
 
 
-def delete(id, index='address-book', doc_type='contact'):
+def delete(index, doc_type, id):
     """Deletes document in the data store."""
 
     es.delete(
         index=index,
         doc_type=doc_type,
-        id=id.lower()
+        id=id
     )
-    return True
 
 
-def checkBounds(data):
-    """Checks the field lengths"""
+def search(index, doc_type, field, query, offset, size):
+    """Searches documents based on query string query in the data store"""
+
+    res = es.search(
+        index=index,
+        doc_type=doc_type,
+        body={
+            'query': {
+                'query_string': {
+                    'default_field': field,
+                    'query': query
+                }
+            }
+        },
+        from_=offset,
+        size=size
+    )
+
+    result = []
+    for i in range(res['hits']['total']):
+        result.append(res['hits']['hits'][i]['_source'])
+    
+    return result
+
+
+def create_contact(data):
+    """Creates a new contact in the data store."""
+
+    # check if name exists
+    if 'name' not in data or not data['name']:
+        return create_response('"name" field is required.', 400)
+    
+    # check bounds
+    if not check_bounds(data):
+        return create_response('Field(s) too long.', 400)
+    
+    # check if contact name is unique
+    if read_contact(data['name']) is not None:
+        return create_response('Contact name must be unique.', 400)
+
+    # construct the contact document
+    contact = {
+        'name': data['name'],
+        'phone': data['phone'] if 'phone' in data else '',
+        'email': data['email'] if 'email' in data else '',
+        'address': data['address'] if 'address' in data else ''
+    }
+
+    # create new contact
+    create(
+        index='address-book',
+        doc_type='contact',
+        id=contact['name'].lower(),
+        doc=contact
+    )
+
+    # send response
+    return create_response('Contact successfully createad', 201)
+
+
+def read_contact(name):
+    """Reads a unique contact in the data store."""
+
+    # read the data store
+    return read(
+        index='address-book',
+        doc_type='contact',
+        id=name.lower()
+    )
+
+
+def update_contact(name, data):
+    """Updates a unique contact in the data store."""
+
+    # check if the contact exists
+    if read_contact(name) is None:
+        return create_response('Contact does not exist.', 404)
+    
+    # check bounds
+    if not check_bounds(data):
+        return create_response('Field(s) too long.', 400)
+
+    # construct contact
+    contact = {}
+    if 'phone' in data:
+        contact['phone'] = data['phone']
+    if 'email' in contact:
+        contact['email'] = data['email']
+    if 'address' in contact:
+        contact['address'] = data['address']
+    
+    # update contact
+    update(
+        index='address-book',
+        doc_type='contact',
+        id=name.lower(),
+        doc=contact
+    )
+
+    # send response
+    return create_response('Contact succesfully updated.', 200)
+
+
+def delete_contact(name):
+    """Deletes a unique contact in the data store."""
+
+    # check if the contact exists
+    if read_contact(name) is None:
+        return create_response('Contact does not exist.', 404)
+    
+
+    # delete contact
+    delete(
+        index='address-book',
+        doc_type='contact',
+        id=name.lower()
+    )
+
+    # send response
+    return create_response('Contact succesfully deleted.', 200)
+
+
+def search_contacts(page_size, page, query):
+    """Searches based on a query in the data store."""
+
+    # check if page number is valid
+    if page <= 0:
+        return create_response('Invalid page number.', 400)
+    
+    # search the data store
+    return search(
+        index='address-book',
+        doc_type='contact',
+        field='name',
+        query=query,
+        offset=page_size * (page - 1),
+        size=page_size
+    )
+
+
+def create_response(description, status):
+    """Creates a response object"""
+
+    return {
+        'description': description,
+        'status': status
+    }
+
+
+def check_bounds(data):
+    """Checks the field lengths for a contact."""
 
     if (
         'name' in data and len(data['name']) > 40 or
@@ -76,5 +220,5 @@ def checkBounds(data):
         'address' in data and len(data['address']) > 100
         ):
         return False
-    else:
-        return True
+    
+    return True
